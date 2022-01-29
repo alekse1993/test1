bebdada38a4b01966b492839b00f7edb05b9b1c6
alekse1993 <lehana_borisov@mail.ru>
2022-01-29 13:43:40 +0300

    Modify

--- src/main/java/com/company/test/ClientB.java
+++
@@ -1,9 +0,0 @@
-package com.company.test;
-
-public class ClientB implements IMailSender {
-
-    @Override
-    public void sendMessage() {
-        System.out.println("Send msg via VK");
-    }
-}
--- src/main/java/com/company/test/IMailSender.java
+++
@@ -1,5 +0,0 @@
-package com.company.test;
-
-public interface IMailSender {
-    void sendMessage();
-}
